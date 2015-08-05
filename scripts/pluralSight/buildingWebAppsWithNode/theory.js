b214601e592314aa9adcfb153a6880db19d5304e
jeantroiani <jeantroiani@icloud.com>
2015-08-05 06:10:53 +0100

    removed streams.js and add a new folder in pluralsight

---
+++ scripts/pluralSight/buildingWebAppsWithNode/theory.js
@@ -0,0 +1,7 @@
+/*
+Debugging
+	-supervisor(install this utility to help you debug, it opens and node command, avoid shuting the ser ver and running it again for changes).
+	-nodemon (same);  npm install -g nodemon
+		to run it:    nodemon filename.js
+	-node inspector (step process, like debugger)
+*/
